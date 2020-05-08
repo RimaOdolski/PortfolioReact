407044d4bc7a901391e88dd0437822ad6dbbd604
Rima O <rymaothmani@gmail.com>
2020-05-07 21:33:40 -0500

    style header

--- myportfolio/src/Components/Header.js
+++ myportfolio/src/Components/Header.js
@@ -7,7 +7,6 @@ class Header extends Component {
       var name = this.props.data.name;
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
-      var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
@@ -33,8 +32,8 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h1 className="responsive-headline">Hi! My name is <br></br>  {name}.</h1>
+            <h3>I'm an Austin,TX based <span>{occupation}</span>. {description}.</h3>
             <hr />
             <ul className="social">
                {networks}
