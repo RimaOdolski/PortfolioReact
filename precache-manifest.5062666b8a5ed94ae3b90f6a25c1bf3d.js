965e9d344a4674e3a7348337248e71f34e878cac
Rima O <rymaothmani@gmail.com>
2020-05-07 22:12:02 -0500

    Updates

---
+++ precache-manifest.5062666b8a5ed94ae3b90f6a25c1bf3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7004bf9e3ab4be5e176d1451af41ec",
+    "url": "/PortfolioReact/index.html"
+  },
+  {
+    "revision": "ad19b82e2c33905c3c91",
+    "url": "/PortfolioReact/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "c90e3bf7004e26630972",
+    "url": "/PortfolioReact/static/js/2.3ceb352d.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/PortfolioReact/static/js/2.3ceb352d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad19b82e2c33905c3c91",
+    "url": "/PortfolioReact/static/js/main.cd7ae20a.chunk.js"
+  },
+  {
+    "revision": "9cc9b40f3961fb3b32bc",
+    "url": "/PortfolioReact/static/js/runtime-main.87ff3ad7.js"
+  }
+]);
